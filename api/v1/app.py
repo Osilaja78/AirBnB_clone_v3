94c307a174460a3cceca9041c1bb79fd16d3ea37
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-29 21:22:23 +0100

    code for task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Root file for flask app.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -18,6 +18,13 @@ def close_storage(self):
     """Closes storage"""
 
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """Handler for 404 errors"""
+
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
