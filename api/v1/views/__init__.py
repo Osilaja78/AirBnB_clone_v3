f5a2f84436f6deb782eb4e99144e1c00bde15261
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-29 22:59:17 +0100

    fixed /views/__init__.py merge conflict

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,11 +10,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-<<<<<<< HEAD
-=======
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
->>>>>>> 8fc6826526df2a8cfc0977df0820257058fe2b6e
