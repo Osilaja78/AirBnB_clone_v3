951c726feab667f1b5f806f0217d2d47c97c4fde
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-10-29 21:14:56 +0100

    code for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -10,3 +11,22 @@ def status():
     """Returns the API status"""
 
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns count for all classes"""
+
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+
+    obj_count = {}
+    for cls in classes:
+        obj_count[cls] = storage.count(classes[cls])
+    
+    return jsonify(obj_count)
